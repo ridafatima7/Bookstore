3a2f22bf7a2395cc8cb161b4ad69e1f12e069f51
ridafatima7 <rida.bsse3962@iiu.edu.pk>
2024-07-20 20:31:54 +0500

    frontend completed

--- Frontend/project/src/Components/Login.jsx
+++ Frontend/project/src/Components/Login.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Login = () => {
+    
     return (
         <>
         <div>
             <dialog id="my_modal_3" className="modal">
                 <div className="modal-box">
                     <form method="dialog">
-                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                        <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
                     </form>
                     <h3 className="font-bold text-lg">Login</h3>
-                    <div className='mt-4 space-y-4'>
+                    <div className='mt-4 space-y-2'>
                         <span>
                             Email
                         </span>
@@ -18,7 +19,7 @@ const Login = () => {
                         <input type='email' placeholder='Enter Email'
                          className='w-80 px-3 py-1 border rounded-md outline-none' />
                          </div>
-                        <div className='mt-4 space-y-4'>
+                        <div className='mt-4 space-y-2'>
                           <span>
                            Password
                         </span>
@@ -28,7 +29,7 @@ const Login = () => {
                     </div>
                     <div className='flex justify-between mt-4'>
                         <button className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-300'>Login</button>
-                        <p>Not registered?<span className='underline text-blue-500 cursor-pointer'>SignUp</span></p>
+                        <p>Not registered?<Link to='/signup'><span className='underline text-blue-500 cursor-pointer'>SignUp</span></Link></p>
                     </div>
                 </div>
             </dialog>
--- Frontend/project/src/Components/SignUp.jsx
+++ Frontend/project/src/Components/SignUp.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import Login from './Login'
 const SignUp = () => {
   return (
-    <div>
-      
+    <div className='flex h-screen items-center justify-center'>
+      <div id="my_modal_6" >
+        <div className="modal-box">
+          <form method="dialog">
+            <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
+          </form>
+          <h3 className="font-bold text-lg">SignUp</h3>
+          <div className='mt-4 space-y-2'>
+            <span>
+              Name
+            </span>
+            <br />
+            <input type='text' placeholder='Enter Name'
+              className='w-70 px-3 py-1 border rounded-md outline-none' style={{ width: "100% " }} />
+          </div>
+          <div className='mt-4 space-y-2'>
+            <span>
+              Email
+            </span>
+            <br />
+            <input type='email' placeholder='Enter Email'
+              className='w-70 px-3 py-1 border rounded-md outline-none' style={{ width: "100% " }} />
+          </div>
+          <div className='mt-4 space-y-2'>
+            <span>
+              Password
+            </span>
+            <br />
+            <input type='password' placeholder='Enter Password'
+              className='w-70 px-3 py-1 border rounded-md outline-none' style={{ width: "100% " }} />
+          </div>
+          <div className='flex justify-between mt-4'>
+            <button className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-300'>SignUp</button>
+            <p>
+              Have Account?
+              <button onClick={() => document.getElementById('my_modal_3').showModal()} className='underline text-blue-500 cursor-pointer'>
+                Login
+              </button>
+              <Login />
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
